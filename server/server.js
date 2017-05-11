cddc4e9930d8a75d922d0685afc3c85254727822
“luissoliveira” <luissoliveira@deloitte.pt>
2017-05-11 15:33:58 +0100

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -15,6 +15,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage',{
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime(),
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime(),
+  });
+
   // Socket.emit('newEmail', {
   //   from: 'mike@example.com',
   //   text: 'Hey. What is going on.',
@@ -38,6 +50,12 @@ io.on('connection', (socket) => {
       text: message.text,
       createdAt: new Date().getTime(),
     });
+
+    // Socket.broadcast.emit('newMessage', {
+    //     from: message.from,
+    //     text: message.text,
+    //     createdAt: new Date().getTime(),
+    //   })
   });
 
   socket.on('disconnect', () => {
