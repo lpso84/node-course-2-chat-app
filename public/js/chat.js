bce4a3cded545260d299b1abe32893f43995c16d
“luissoliveira” <luissoliveira@deloitte.pt>
2017-05-15 14:31:20 +0100

    commit do luis para o nuno

--- public/js/chat.js
+++ public/js/chat.js
@@ -20,7 +20,16 @@ function scrollToBottom() {
 
 
 socket.on('connect', function() {
-  console.log('Connected to server');
+  var params = jQuery.deparam(window.location.search);
+
+  socket.emit('join', params, function(err) {
+    if (err) {
+      alert(err);
+      window.location.href = '/'
+    } else {
+      console.log('No error');
+    }
+  })
 });
 // Socket.emit('createEmail', {
 //   to: 'jen@example.com',
---
+++ server/utils/validation.test.js
@@ -0,0 +1,20 @@
+/*jshint esversion: 6 */
+const expect = require('expect');
+const {isRealString} = require('./validation.js');
+
+describe('isRealString', () => {
+  it('should reject non-string values', () => {
+    var res = isRealString(98);
+    expect(res).toBe(false);
+  });
+
+  it('should reject string with only spaces', () => {
+    var res = isRealString('    ');
+    expect(res).toBe(false);
+  });
+
+  it('should allow string with non-space characters', () => {
+    var res = isRealString('    Luis    ');
+    expect(res).toBe(true);
+  });
+});
